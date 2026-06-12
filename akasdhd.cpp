c5cf5126ec5617c78742ef30957c5f06d1fde66d
Akash67-56 <akashsivabhag@gmail.com>
2026-06-12 20:48:04 +0530

    Add files via upload

---
+++ akasdhd.cpp
@@ -0,0 +1,223 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <ctime>
+
+using namespace std;
+
+bool gameOver;
+const int width = 20;
+const int height = 20;
+
+int x, y;
+int fruitX, fruitY;
+int score;
+
+int tailX[100], tailY[100];
+int nTail;
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+
+    x = width / 2;
+    y = height / 2;
+
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+
+    score = 0;
+    nTail = 0;
+
+    // Hide Cursor
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.dwSize = 100;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(console, &cursorInfo);
+}
+
+void Draw()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD cursorPos;
+    cursorPos.X = 0;
+    cursorPos.Y = 0;
+
+    SetConsoleCursorPosition(hConsole, cursorPos);
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+
+            if (i == y && j == x)
+            {
+                cout << "O";
+            }
+            else if (i == fruitY && j == fruitX)
+            {
+                cout << "F";
+            }
+            else
+            {
+                bool printTail = false;
+
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        printTail = true;
+                        break;
+                    }
+                }
+
+                if (!printTail)
+                    cout << " ";
+            }
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+
+    cout << "\nScore: " << score;
+    cout << "\nControls: W A S D | Exit: X\n";
+}
+
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+        case 'A':
+            dir = LEFT;
+            break;
+
+        case 'd':
+        case 'D':
+            dir = RIGHT;
+            break;
+
+        case 'w':
+        case 'W':
+            dir = UP;
+            break;
+
+        case 's':
+        case 'S':
+            dir = DOWN;
+            break;
+
+        case 'x':
+        case 'X':
+            gameOver = true;
+            break;
+        }
+    }
+}
+
+void Logic()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+
+    case RIGHT:
+        x++;
+        break;
+
+    case UP:
+        y--;
+        break;
+
+    case DOWN:
+        y++;
+        break;
+
+    default:
+        break;
+    }
+
+    // Wall Collision
+    if (x < 0 || x >= width || y < 0 || y >= height)
+        gameOver = true;
+
+    // Tail Collision
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
+
+    // Eat Fruit
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+
+        nTail++;
+    }
+}
+
+int main()
+{
+    srand(time(0));
+
+    Setup();
+
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+
+        Sleep(120);
+    }
+
+    cout << "\n\nGame Over!";
+    cout << "\nFinal Score: " << score << endl;
+
+    return 0;
+}
